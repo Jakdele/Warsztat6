78c1b032c8755198cc01957604d81055c69441af
Jakub Delekta <jakdele@protonmail.com>
2018-08-26 23:47:57 +0200

    Changed query to match task description.

--- src/main/java/pl/coderslab/repository/TweetRepository.java
+++ src/main/java/pl/coderslab/repository/TweetRepository.java
@@ -1,5 +1,6 @@
 package pl.coderslab.repository;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,7 +11,12 @@ import java.util.List;
 
 public interface TweetRepository extends JpaRepository<Tweet, Integer> {
     List<Tweet> findAllByUser(User user);
+
     List<Tweet> findAllByUserId(int userId);
+
+    @Query("SELECT t FROM Tweet t order by t.created desc ")
+    List<Tweet> findAllSorted();
+
     void removeAllByUserId(int userId);
 
 }
